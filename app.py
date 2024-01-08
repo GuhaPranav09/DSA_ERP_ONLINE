beb731f60bc5992d79019e04d67b9bfa1d40ce10
GuhaPranav09 <yg.pranav@gmail.com>
2024-01-08 23:31:41 +0530

    a

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def manager_login():
         sitenum = request.form['sitenum']
         username = request.form['username']
         password = request.form['password']
-        print(mysql.connection)
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM vitproject.login WHERE Site =%s AND username = %s AND password = %s', (sitenum, username, password,))
         account = cursor.fetchone()
@@ -39,40 +38,29 @@ def manager_login():
         else:
             msg = 'Incorrect username/password!'
         
-    return render_template('manager_login.html')
+    return render_template('manager_login.html',msg=msg)
 
-@app.route('/manager_register', methods=['GET','POST'])
+@app.route('/director_login', methods=['GET','POST'])
 def director_login():
 
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'sitenum' in request.form:
         username = request.form['username']
         password = request.form['password']
-        sitenum = request.form['sitenum']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM vitproject.login WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM vitproject.login WHERE Site = %s AND username = %s AND password = %s', (0, username, password,))
         account = cursor.fetchone()
         if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[0-9]', sitenum):
-            msg = 'Invalid site number!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not sitenum:
-            msg = 'Please fill out the form!'
+            session['loggedin'] = True
+            session['password'] = account['password']
+            session['username'] = account['username']
+            return render_template('director_home.html',msg=session['username'])
         else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, sitenum,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered!'
-            
-    elif request.method == 'POST':
-        msg = 'Please fill out the form!'
+            msg = 'Incorrect username/password!'
 
-    return render_template('manager_register.html',msg=msg)
+    return render_template('director_login.html',msg=msg)
 
-@app.route('/manager_home')
-def manager_home():
-    return render_template('manager_home.html', msg=session['sitenum'])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
