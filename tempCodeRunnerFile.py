f0dd7a71a4c3b10b47874c7f9023f83a62b12d6c
GuhaPranav09 <yg.pranav@gmail.com>
2024-05-27 14:13:27 +0530

    j

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-@app.route('/')
-def index():
-    cur = conn.cursor(pymysql.cursors.DictCursor)
-    cur.execute("SELECT DISTINCT Site as site_number FROM expenditure")
-    sites = cur.fetchall()
-    return render_template('calendar.html', sites=sites)
